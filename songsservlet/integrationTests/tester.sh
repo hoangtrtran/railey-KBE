8a8f7414f677c6728874705dfadc85673e3680de
hoangtrtran <hoangtrtran@gmail.com>
2019-11-20 13:05:59 +0100

    add integrationTests

---
+++ songsservlet/integrationTests/tester.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+#
+# Usage:
+# ./tester.sh TEAMNAME 
+# Example:
+# ./tester.sh teames
+# 
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    echo "Usage: ./tester.sh TEAMNAME"
+    echo "Example: ./tester.sh teames"
+    exit 1
+fi
+
+echo "--- REQUESTING SONG NUMBER 6 IN XML --------"
+curl -X GET \
+     -H "Accept: application/xml" \
+     -v "http://localhost:8080/songsservlet-railey/songs?songId=6"
+echo "---"
+
+echo "--- REQUESTING SONG NUMBER 6 IN JSON --------"
+curl -X GET \
+     -H "Accept: application/json" \
+     -v "http://localhost:8080/songsservlet-railey/songs?songId=6"
+echo "---"
+
+echo "--- PUTTING SONG 6 IN JSON ------------------"
+curl -X PUT \
+     -H "Content-Type: application/json" \
+     -d "@einSong.json" \
+     -v "http://localhost:8080/songsservlet-railey/songs?songId=6"
+echo "---"
+
+#echo "--- SHUTTING DOWN TOMCAT -----------"
+#$CATALINA_HOME/bin/shutdown.sh
--- songsservlet/src/main/java/htwb/ai/railey/controller/SongsServlet.java
+++ songsservlet/src/main/java/htwb/ai/railey/controller/SongsServlet.java
@@ -1,61 +1,42 @@
 package htwb.ai.railey.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Enumeration;
-
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.io.IOUtils;
-
+/**
+ * Servlet implementation class SongsServlet
+ */
+@WebServlet("/songs")
 public class SongsServlet extends HttpServlet {
-
 	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SongsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-	private String uriToDB = null;
-
-	@Override
-	public void init(ServletConfig servletConfig) throws ServletException {
-		this.uriToDB = servletConfig.getInitParameter("songs.json");
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println(request);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	@Override
-	public void doGet(HttpServletRequest request, 
-	        HttpServletResponse response) throws IOException {
-		// alle Parameter (keys)
-		Enumeration<String> paramNames = request.getParameterNames();
-
-		String responseStr = "";
-		String param = "";
-		while (paramNames.hasMoreElements()) {
-			param = paramNames.nextElement();
-			responseStr = responseStr + param + "=" 
-			+ request.getParameter(param) + "\n";
-		}
-		response.setContentType("text/plain");
-		try (PrintWriter out = response.getWriter()) {
-			responseStr += "\n Your request will be sent to " + uriToDB;
-			out.println(responseStr);
-		}
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
-	@Override
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.setContentType("text/plain");
-		ServletInputStream inputStream = request.getInputStream();
-		byte[] inBytes = IOUtils.toByteArray(inputStream);
-		try (PrintWriter out = response.getWriter()) {
-			out.println(new String(inBytes));
-		}
-	}
-	*/
-	protected String getUriToDB () {
-		return this.uriToDB;
-	}
 }

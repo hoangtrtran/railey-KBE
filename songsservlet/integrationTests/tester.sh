547ff3288c132702d06f9c1385eb35d3d4afdfe2
hoangtrtran <hoangtrtran@gmail.com>
2019-11-24 22:54:12 +0100

    update web.xml add methode to songsservlet doPut doGet

--- songsservlet/integrationTests/tester.sh
+++ songsservlet/integrationTests/tester.sh
@@ -26,9 +26,32 @@ curl -X GET \
      -v "http://localhost:8080/songsservlet-railey/songs?songId=6"
 echo "---"
 
+echo "--- REQUESTING SONG NUMBER 6 IN JSON --------"
+curl -X GET \
+     -v "http://localhost:8080/songsservlet-railey/songs?songId=6"
+echo "---"
+
+echo "--- REQUESTING SONG NUMBER 6 IN JSON --------"
+curl -X GET \
+    -H "Accept: */*" \
+     -v "http://localhost:8080/songsservlet-railey/songs?songId=6"
+echo "---"
+
+echo "--- REQUESTING SONG NUMBER 6 IN JSON --------"
+curl -X GET \
+    -H "Accept: plain/text" \
+     -v "http://localhost:8080/songsservlet-railey/songs?songId=6"
+echo "---"
+
 echo "--- PUTTING SONG 6 IN JSON ------------------"
 curl -X PUT \
      -H "Content-Type: application/json" \
+     -d "@einSong.json" \
+     -v "http://localhost:8080/songsservlet-railey/songs?songId=6"
+echo "---"
+
+echo "--- PUTTING SONG 6 IN JSON ------------------"
+curl -X PUT \
      -d "@einSong.json" \
      -v "http://localhost:8080/songsservlet-railey/songs?songId=6"
 echo "---"
